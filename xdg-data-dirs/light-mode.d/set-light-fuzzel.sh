4c7cbd1a72e881671b7c7d54b079581fa164131f
Lou <luigis816@gmail.com>
2025-07-30 18:01:18 +0200

    No that was definitely something else

--- xdg-data-dirs/light-mode.d/set-light-fuzzel.sh
+++ xdg-data-dirs/light-mode.d/set-light-fuzzel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp /home/<lou>/.config/fuzzel/fuzzel-light.ini /home/<lou>/.config/fuzzel/fuzzel.ini
+cp /home/<user>/.config/fuzzel/fuzzel-light.ini /home/<user>/.config/fuzzel/fuzzel.ini
